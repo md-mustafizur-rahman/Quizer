70f2161daa55d8a5afc53b67382e71ff13e13c4e
md-mustafizur-rahman <mustafizur.se@gmail.com>
2024-03-17 22:57:11 +0600

    add category

--- src/main/java/quizer/dreamzoneit/com/quizer/Controller/admin/CategoryController.java
+++ src/main/java/quizer/dreamzoneit/com/quizer/Controller/admin/CategoryController.java
@@ -2,27 +2,42 @@ package quizer.dreamzoneit.com.quizer.Controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import quizer.dreamzoneit.com.quizer.Model.Category;
 import quizer.dreamzoneit.com.quizer.repository.CategoryRepo;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
 
     @Autowired
     CategoryRepo categoryRepo;
-    @RequestMapping(value = "add-category",method = RequestMethod.GET)
-    public  String add_category(){
-        return "admin/pages/category/category";
+    @RequestMapping(value = "add-category", method = RequestMethod.GET)
+    public ModelAndView getCategoryForm() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        // Fetch categories from the repository
+        List<Category> categories = categoryRepo.findAll();
+        modelAndView.addObject("categories", categories);
+        modelAndView.setViewName("admin/pages/category/category");
+        return modelAndView;
     }
 
     @RequestMapping(value = "store-category", method = RequestMethod.POST)
     public  String store_category(@ModelAttribute Category category){
 
        categoryRepo.save(category);
-        return "admin/pages/category/category";
+        return "redirect:add-category";
 
     }
+    @RequestMapping(value = "delete-category/{id}", method = RequestMethod.GET)
+    public  String delete_category(@PathVariable int id){
+        categoryRepo.deleteById(id);
+        return "redirect:/add-category";
+
+    }
+
+
 }
